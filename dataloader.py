925f1d94f42dd9a3964afb52e862bd6f9704bb77
Adriane Kelle <100316502+adrikll@users.noreply.github.com>
2025-05-06 20:46:58 -0300

    dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -32,9 +32,15 @@ class ChagasDataset(Dataset):
 
     def _extract_ecg_features(self, signal: np.ndarray) -> pd.Series:
         max_samples = int(self.max_minutes * 60 * self.sampling_rate)
+
+        if len(signal) < max_samples:
+            raise ValueError(f"Sinal muito curto: tem {len(signal)/self.sampling_rate/60:.1f} min, necessário >= {self.max_minutes} min.")
+        
         segment = signal[:max_samples]
 
-        signals_proc, info = nk.ecg_process(segment, sampling_rate=self.sampling_rate)
+        #limpeza e detecção de picos com método pantompkins1985
+        clean_ecg = nk.ecg_clean(segment, sampling_rate=self.sampling_rate, method="pantompkins1985")
+        _, info = nk.ecg_peaks(clean_ecg, sampling_rate=self.sampling_rate, method="pantompkins1985")
 
         try:
             hrv_time = nk.hrv_time(info['ECG_R_Peaks'], sampling_rate=self.sampling_rate, show=False)
@@ -57,6 +63,11 @@ class ChagasDataset(Dataset):
             print(e)
             hrv_nonlinear = pd.DataFrame()
 
+        
+        if 'HRV_ULF' in hrv_freq.columns:
+            hrv_freq = hrv_freq.drop(columns=['HRV_ULF'])
+        
+
         features_df = pd.concat([hrv_time, hrv_freq, hrv_nonlinear], axis=1)
 
         if features_df.empty:
@@ -72,7 +83,7 @@ class ChagasDataset(Dataset):
         basename = os.path.basename(path)
         m = re.match(r'sinal(\d+)\.txt', basename)
         if not m:
-            raise ValueError(f"Arquivo com nome inválido: {basename}")
+            raise ValueError(f"Arquivo inválido: {basename}")
         patient_id = int(m.group(1))
 
         info = self.patients_df[self.patients_df['ID'] == patient_id]
@@ -107,7 +118,6 @@ def save_features(dataset, output_file):
 
             obito = patient_info['Obito_MS'].values[0]
             tempo = patient_info['Time'].values[0]
-            class_1 = 1 if (obito == 1 & tempo < 5) else 0
 
             feature_dict = {name: features[i].item() for i, name in enumerate(dataset.feature_names)}
             feature_dict.update({
@@ -116,7 +126,7 @@ def save_features(dataset, output_file):
                 'patient_id': patient_id,
                 'Obito_MS': obito,
                 'Time': tempo,
-                'class_1': class_1
+                'class_1': event.item()  
             })
 
             df_row = pd.DataFrame([feature_dict])
@@ -143,7 +153,6 @@ if __name__ == "__main__":
         weights = json.load(f)
 
     ds = ChagasDataset('base_chagas', df_chagas, weights=weights, return_ids=True)
-
 
     save_features(ds, 'features_chagas.csv')
 
